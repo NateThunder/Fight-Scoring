2056ddd7dc480f961f79aaa6c60ea89209edbbee
NateThunder <stoneyexperiment@gmail.com>
2025-10-11 23:13:02 +0100

    update judgered and judging

--- JudgeReg.sol
+++ JudgeReg.sol
@@ -6,13 +6,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract JudgeReg is Ownable {
-// Constructor to link to openzepplin code ==================    
+// Constructor to link to openzepplin code ============================    
     constructor() Ownable(msg.sender) {}
 
-// Event to be able to oeasili be seen on system =============
+// Event to be able to oeasili be seen on system ======================
     event NewJudge(uint judgeId, string judgeName);
 
-// Getting the databse of fighters and judges ==================
+// Getting the databse of fighters and judges =========================
     struct Judge {
         string name;
         uint16 rank;
@@ -20,11 +20,12 @@ contract JudgeReg is Ownable {
         uint16 wrongDecision;
     }
 
-// defining an array for MMA fighters =================
+// defining an array for MMA fighters ==================================
     Judge[] public judges;
 
-// Mapping of users to ID number =============================
-    mapping (uint => address) public judgeToOwner; 
+// Mapping of users to ID number =======================================
+    mapping (uint => address) public judgeToOwner;
+    mapping (address => uint) public ownerToJudge;
     mapping (address => uint256) judgeBalance;
 
 // Setting modifier for the owner of for the judges ====================
@@ -35,10 +36,12 @@ contract JudgeReg is Ownable {
     }
 
 // Register judge ====================================================
-    function _registerJudge(string memory _name) public  {
+    function _registerJudge(string memory _name) public  { 
+        require(ownerToJudge[msg.sender] == 0 && judgeToOwner[0] != msg.sender, "Judge already exsists"); // prevents multiple judges per address 
         judges.push(Judge(_name, 1, 0, 0));
         uint256 id = judges.length - 1;
         judgeToOwner[id] = msg.sender;
+        ownerToJudge[msg.sender] = id;
         emit NewJudge(id, _name);
     }
 }
--- Scoring.sol
+++ Scoring.sol
@@ -4,39 +4,50 @@ pragma solidity ^0.8.26;
 
 import "./MatchReg.sol";
 
-// contract for scoring the fight =============================
+// contract for scoring the fight ===========================================================
 
 contract Scoring is MatchReg {
 
-    event FightScore(uint fightId, uint roundId, uint8 scoreA, uint8 scoreB);
+    event FightScore(uint fightId, uint roundId, uint8 scoreA, uint8 scoreB, uint judgeId);
+
+// struct for judges scorecard ===============================================================
 
     struct Fight {
         uint fightId;
         uint roundId;
         uint fighterAScore;
         uint fighterBScore;
+        uint judgeId;
     }
 
-    Fight[] private fights;    
+    Fight[] private fights;    // array of fight struct
+    
+    
+// Modifier to make sure score is within limit ------------------------------------------------
 
     modifier scoreLimit(uint _scoreA, uint8 _scoreB){
         require(_scoreA < 11 && _scoreB < 11, "maximum Score is 10");
         _;
     }
 
-    function judgeScore(uint _fightId, uint _roundId, uint8 _scoreA, uint8 _scoreB) public scoreLimit(_scoreA, _scoreB) {
+// Function for the score card  -----------------------------------------------------------------
+
+    function judgeScore(uint _fightId, uint _roundId, uint8 _scoreA, uint8 _scoreB, uint _judgeId) public scoreLimit(_scoreA, _scoreB) onlyJudge(_judgeId) {
         require(_roundId < matches[_fightId].rounds, "Invalid Rounds");
         fights.push(Fight({
             fightId: _fightId,
             roundId: _roundId,
             fighterAScore: _scoreA,
-            fighterBScore: _scoreB
+            fighterBScore: _scoreB,
+            judgeId: _judgeId
         }));
 
-        emit FightScore(_fightId, _roundId, _scoreA, _scoreB);
+        emit FightScore(_fightId, _roundId, _scoreA, _scoreB, _judgeId); // emits scorcard information
     }
 
-    function winner(uint _fightId) public view returns (uint, string memory) {
+// function to calvulate the winner based on the scorecard ------------------------------------------
+
+    function winner(uint _fightId) public view returns (uint, string memory) { // view function for gas saving
         uint scoreA = 0;
         uint scoreB = 0;
         uint winnerId;
@@ -48,7 +59,7 @@ contract Scoring is MatchReg {
                 scoreB += fights[x].fighterBScore;
             }
         }
-
+     
         if (scoreA > scoreB) {
             winnerId = matches[_fightId].fighterAId;
             winnerName = (fighters[winnerId].name);
